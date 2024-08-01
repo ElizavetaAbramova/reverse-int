24d01556703980880ac3ba40fcb9e70b9f4e1a48
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2024-08-01 22:37:43 +0400

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+
+  let result = '';
+  let number = Math.abs(n).toString();
+  for (let i = 0; i < number.length; i++) {
+
+    result = `${number[i]}${result}`;
+  }
+
+  return result;
 }
